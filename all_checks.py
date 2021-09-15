b5480c7f8b05c664de5721e8b13d7bc8c15c1eb3
Alex <alex.benavente@outlook.com>
2021-09-15 15:20:49 -0400

    Added network and root partition functions.

--- all_checks.py
+++ all_checks.py
@@ -2,7 +2,10 @@
 
 import os
 import shutil
+import socket
 import sys
+import psutil
+
 
 def check_reboot():
     """Returns True if the computer has a pending reboot."""
@@ -20,16 +23,37 @@ def check_disk_full(disk, min_gb, min_percent):
     else:
         return False
 
-def main():
-    if check_reboot():
-        print("Pending Reboot.")
-        sys.exit(1)
-    if check_disk_full("/", 2, 10):
-        print("Disk full.")
-        sys.exit(1)
+def check_root_full():
+    """Returns True if the root partition is full, False otherwise"""
+    return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
+def check_no_network():
+    """Returns True if it fails to resolve Google's URL, False otherwise"""
+    try:
+        socket.gethostbyname("www.google.com")
+        return False
+    except:
+        return True
+def check_cpu_constrained():
+    """Returns True if the CPU is having too much usage, False otherwise"""
+    return psutil.cpu_percent(1) > 75
+
+def main():
+    checks=[
+        (check_reboot, "Pending Reboot."),
+        (check_root_full, "Root partition full"),
+        (check_cpu_constrained, " CPU load too high"),
+        (check_no_network, "No working network")
+    ]
+    everything_ok = True
+    for check, msg in checks:
+        if check():
+            print(msg)
+            everything_ok = False
+
+    if not everything_ok:
+        sys.exit(1)
     else:
         print("Everything OK!")
         sys.exit(0)
-
 main()
